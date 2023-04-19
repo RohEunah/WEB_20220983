e10ef25e49324e6b9baf17cf5b389fb516093e41
노은아 <shdmsdk2012@gmail.com>
2023-04-19 05:16:24 +0000

    ##2023년 4월 19일 7주 홈페이지 수정 완료!

---
+++ js/arry_test2.js
@@ -0,0 +1,15 @@
+var arr = ['a','b','c'];
+var arr2 = [1,'b',3.5151231231];
+// arr =['a','b','c','e'];
+arr[arrlength] = 'e';
+arr.arrlength = arr.length - 1;
+arr[5] = 'g'
+arr.pop();
+arr2.push("pushed");
+arr.shift();
+arr2.unshift("pushed2");
+
+console.log(arr);
+console.log(typeof_arr2[0]);
+console.log(arr2.toString());
+console.log(typeof_arr[3]); 
--- js/search.js
+++ js/search.js
@@ -1,9 +1,16 @@
 document.getElementById("search_btn").addEventListener('click', search_message);
 
-function search_message(){
-   alert("검색을 수행합니다!"); 
-	let search_str = document.querySelector("#search_txt"); // 변수에 저장
-	document.getElementById("search_message").innerHTML = search_str.value; // 태그에 값 추가
-	console.log(search_str.value); // 콘솔에 출력
+var search_arry =[]; //빈 배열 - 전역 변수
 
+function search_message(){
+	let search_str = document.querySelector("#search_txt");
+	if(search_str.value.length === 0){
+		alert("검색어가 비었습니다. 입력해주세요");
+	}
+	else{
+		alert("검색어를 수행합니다!");
+		search_arry.push(search_str.value); // 배열에 검색어 추가
+		let text = document.getElementById("search_message").innnerHTML = search_arry.tostring();//값 변환
+		document.querySelector("#from_main").submut();
+	}
 }
